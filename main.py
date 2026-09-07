9e8b3a17b3eb0a224038b4b03b5259cb83e907a4
lussy21 <giannisg7828@gmail.com>
2026-09-07 19:08:46 +0300

    Fix formatting in print statement for match details

--- main.py
+++ main.py
@@ -64,9 +64,9 @@ for asian in event_odds:
             "| odds2:", asian.get("odds2")
         )
 print(
-    home, "vs", away,
-    "| 1:", odd1,
-    "| 2:", odd2,
-    "| ΦΑΒΟΡΙ:", favorite,
-    "| event:", event_id
+home, "vs", away,
+"| 1:", odd1,
+"| 2:", odd2,
+"| ΦΑΒΟΡΙ:", favorite,
+"| event:", event_id
 )
