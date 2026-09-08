1b9535600e1f13e93597bf93f244ca3234ef7283
lussy21 <giannisg7828@gmail.com>
2026-09-08 16:58:02 +0300

    Refactor row insertion when event ID is not found
    Update row insertion logic to use SHEET.update() method.

--- main.py
+++ main.py
@@ -173,11 +173,13 @@ for match in matches:
             row_number = i
             break
 
-    if row_number is None:
-        SHEET.append_row([
-            "", home, away, fav_side,
-            "", "", "",
-            "", "", "",
-            "", "", "", "", "",
-"", "", event_id_text        ])
-    
+       if row_number is None:
+        row_number = max(3, len(SHEET.col_values(2)) + 1)
+        SHEET.update(
+            range_name=f"A{row_number}:R{row_number}",
+            values=[[
+                "", home, away, fav_side,
+                "", "", "", "", "", "", "", "", "", "",
+                "", "", "", event_id_text
+            ]]
+        )
