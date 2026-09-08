1340ae874d2e20a1333277aeedb0ee210a12d73e
lussy21 <giannisg7828@gmail.com>
2026-09-08 14:59:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ for match in matches:
 
     row_number = None
     for i, row in enumerate(rows[1:], start=2):
-        if len(row) >= 16 and row[15] == event_id_text:
+        if len(row) >= 17 and row[16] == event_id_text:
             row_number = i
             break
 
